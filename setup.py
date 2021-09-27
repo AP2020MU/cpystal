8a127456b0aa1d6b8189ff4f805446a0467482a0
AP2020MU <ut.uto.qk6284963@gmail.com>
2021-09-27 10:32:49 +0900

    make a new file: 'requirements.txt' and change 'setup.py'

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
 setup(
     name="cpystal",
     version="0.0.0",
-    description='This is a Python package for dealing with crystals, experimental data of physical property.',
-    packages=find_packages(where='cpystal'),
-    package_dir={'': 'cpystal'},
+    description="This is a Python package for dealing with crystals and experimental data of physical property.",
+    packages=find_packages(where="cpystal"),
+    package_dir={"": "cpystal"},
+    install_requires=_requires_from_file("requirements.txt")
 )
