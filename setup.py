c19c385f9fa57822d5adf8119e8c5a1c3ec9f5f8
AP2020MU <ut.uto.qk6284963@gmail.com>
2021-09-27 23:33:22 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,7 @@ setup(
     description="This is a Python package for dealing with crystals and experimental data of physical property.",
     packages=find_packages(where="cpystal"),
     package_dir={"": "cpystal"},
-    install_requires=_requires_from_file("requirements.txt")
+    install_requires=_requires_from_file("requirements.txt"),
+    author="AP2020MU",
+    url='https://github.com/AP2020MU/cpystal',
 )
