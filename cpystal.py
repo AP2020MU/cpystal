0b1894c31526c7095f012906f6534ff2f9869163
AP2020MU <ut.uto.qk6284963@gmail.com>
2021-10-06 16:11:34 +0900

    add new functions: 'atoms_position_from_p1_file' and 'make_struct_file'

--- cpystal.py
+++ cpystal.py
@@ -1258,6 +1258,64 @@ def ingredient_flake_dp(A: List[int], W: int) -> None: # A: 適当に整数化�
         print(W+k, ans)
     return
 
+def atoms_position_from_p1_file(p1_filename: str) -> List[List[str]]:
+    """Get the position of atoms in unit cell from ".p1" file.
+
+    Note:
+        Atomic coordinates in the ".p1" file must be written as "Fractional coordinates".
+
+    Args:
+        p1_filename (str): Name of the p1 file.
+    
+    Returns:
+        (List[List[str]]): list of [`atom_name`, `X`, `Y`, `Z`, `Occupation`]
+    """
+    with open(p1_filename) as f:
+        lines: List[str] = f.readlines()
+    idx: int = lines.index("Direct\n")
+    res: List[List[str]] = []
+    for i in range(idx+1,len(lines)):
+        line: List[str] = lines[i].split()
+        atom: str = re.sub(r"\d", "", line[3])
+        XYZOccupation: List[str] = line[:3] + line[4:5]
+        res.append([atom, *XYZOccupation])
+    return res
+
+def make_struct_file(cif_filename: str, p1_filename: str) -> str:
+    """Make a ".struct" file from ".cif" file and ".p1" file.
+
+    Note:
+        Atomic coordinates in the ".p1" file must be written as "Fractional coordinates".
+        The ".struct" file will be saved in the same directory as ".cif" file.
+
+    Args:
+        cif_filename (str): Name of the cif file.
+        p1_filename (str): Name of the p1 file.
+    
+    Returns:
+        (str): The content of saved ".struct" file.
+    """
+    material: Crystal = Crystal.from_cif(cif_filename)
+    positions: List[str] = ["\t".join(line) for line in atoms_position_from_p1_file(p1_filename)]
+    res: List[str] = ["! Text format structure file",
+    "",
+    f"{material.name}\t\t\t! Crystal Name",
+    f"{material.a}\t{material.b}\t{material.c}\t! Lattice constants a, b, c (�)",
+    f"{material.alpha}\t{material.beta}\t{material.gamma}\t! alpha, beta, gamma (degree)",
+    "",
+    f"{material.fu_per_unit_cell * int(sum(material.components.values()))}\t! Total number of atoms in the unit cell",
+    "",
+    "! Atom	X	Y	Z	Occupation",
+    *positions,
+    "",
+    "0	! Debye characteristic temperature (K)",
+    "0	! Thermal expansion coefficient (10^-6/K)"]    
+    
+    with open(f"{cif_filename.replace('.cif', '')}.struct", 'w') as f:
+        f.write("\n".join(res))
+    return "\n".join(res)
+    
+
 def main() -> None:
     pass
     return
