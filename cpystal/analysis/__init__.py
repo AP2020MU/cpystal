8e92ae7104c62fcb3afab87c36b1db738476cdd2
AP2020MU <ut.uto.qk6284963@gmail.com>
2021-10-19 00:58:59 +0900

    delete pmg.py and merge it to analysis.py;change the docstring of analysis.py; corresponding these changes, change __init__.py

--- cpystal/analysis/__init__.py
+++ cpystal/analysis/__init__.py
@@ -1,7 +1,5 @@
+from .analysis import compare_powder_Xray_experiment_with_calculation
+from .analysis import make_powder_Xray_diffraction_pattern_in_calculation
+from .analysis import Crystal_instance_from_cif_data
 from .analysis import atoms_position_from_p1_file
 from .analysis import make_struct_file
-
-### loading `pymatgen` has heavy overhead
-# from .pmg import compare_powder_Xray_experiment_with_calculation
-# from .pmg import make_powder_Xray_diffraction_pattern_in_calculation
-# from .pmg import Crystal_instance_from_cif_data
