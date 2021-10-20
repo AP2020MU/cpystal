4e7a8f6e0b900167d46e79d776aa36e3d46929e3
AP2020MU <ut.uto.qk6284963@gmail.com>
2021-10-20 12:19:32 +0900

    bug fix

--- cpystal/core/__init__.py
+++ cpystal/core/__init__.py
@@ -2,4 +2,4 @@ from .core import Crystal as Crystal
 from .core import MPMS as MPMS
 from .core import PPMSResistivity as PPMSResistivity
 from .core import ingredient_flake_dp as ingredient_flake_dp
-from .core import Semimutable_dict as ingredient_flake_dp
+from .core import Semimutable_dict as Semimutable_dict
