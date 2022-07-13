b1028477030878f86b6a5f71d56d9f1c4a61fe34
AP2020MU <ut.uto.qk6284963@gmail.com>
2022-07-13 17:56:10 +0900

    add new functions: brillouin, paramagnetization_curie, fit_paramagnetism, demagnetizating_factor_ellipsoid, demagnetizating_factor_rectangular_prism

--- cpystal/analysis/analysis.py
+++ cpystal/analysis/analysis.py
@@ -25,6 +25,7 @@ import numpy as np
 import pymatgen # type: ignore
 from pymatgen.io.cif import CifParser # type: ignore
 import pymatgen.analysis.diffraction.xrd # type: ignore
+from scipy.optimize import curve_fit
 from scipy.stats import norm
 from scipy import integrate
 
@@ -648,22 +649,196 @@ def cal_Debye_specific_heat(T: float, TD: float, num_atom_per_formula_unit: int)
     return 3 * R * num_atom_per_formula_unit * fD(T/TD)
 
 def cal_thermal_conductivity(material: Crystal, T: float, TD: float, l: float) -> float:
-        """Calculating thermal conductivity of phonon based on Debye model.
+    """Calculating thermal conductivity of phonon based on Debye model.
 
-        Args:
-            material (Crystal): Crystal instance.
-            T (float): Temperature (K).
-            TD (float): Debye temperature (K).
-            l (float): Mean free path (cm).
-        
-        Returns:
-            (float): Thermal conductivity of phonon based on Debye model (WK^-1m^-1).
-        """
-        n: int = material.num_atom_per_formula_unit
-        v: float = material.cal_phonon_velocity(TD) # cm/s
-        C_mol: float = cal_Debye_specific_heat(T, TD=TD, num_atom_per_formula_unit=n) # J/K/mol
-        c: float = C_mol * n / (material.V * material.NA / material.fu_per_unit_cell) # J/K/cm^3
-        return 1/3 * c * v * l * 100. # W/Km
+    Args:
+        material (Crystal): Crystal instance.
+        T (float): Temperature (K).
+        TD (float): Debye temperature (K).
+        l (float): Mean free path (cm).
+    
+    Returns:
+        (float): Thermal conductivity of phonon based on Debye model (WK^-1m^-1).
+    """
+    n: int = material.num_atom_per_formula_unit
+    v: float = material.cal_phonon_velocity(TD) # cm/s
+    C_mol: float = cal_Debye_specific_heat(T, TD=TD, num_atom_per_formula_unit=n) # J/K/mol
+    c: float = C_mol * n / (material.V * material.NA / material.fu_per_unit_cell) # J/K/cm^3
+    return 1/3 * c * v * l * 100. # W/Km
+
+def brillouin(x: float, J: float) -> float:
+    """Brillouin function B_J(x).
+
+    Args:
+        x (float): Real parameter.
+        J (float): Integer of half integer (corresponds to total momentum quantum number).
+    
+    Returns:
+        (float): B_J(x).
+    """
+    return (2*J+1) / (2*J) / np.tanh(x*(2*J+1)/(2*J)) - 1 / (2*J) / np.tanh(x/(2*J))
+
+def paramagnetization_curie(H: float, T: float, g: float, J: float, n: int) -> float:
+    """Magnetization from Curie paramagnetism.
+
+    Note:
+        M = n g J B_J(g muB J H/kB T) [muB/f.u.],
+        where
+            n = number of magnetic atom per formula unit,
+            g = g factor,
+            J = total angular momentum quantum number,
+            B_J = Brillouin function,
+            kB = Boltzmann constant (J/K),
+            H = magnetic field (Oe),
+            T = temperature (K).
+    
+    Args:
+        H (float): Magnetic field (Oe).
+        T (float): Temperature (K).
+        g (float): g-factor.
+        J (float): Total angular momentum quantum number.
+        n (float): Number of magnetic atom per formula unit (1/f.u.).
+    
+    Returns:
+        (float): Magnetization from Curie paramagnetism (muB/f.u.).
+    """
+    muB: float = 9.27401e-21 * 1.0e-7 # Bohr磁子 [emu = erg/Oe = 10^(-7) J/Oe]
+    kB: float = 1.380649e-23 # Boltzmann定数 [J/K]
+    return n * g * J * brillouin(g*J*muB*H/(kB*T), J)
+
+def fit_paramagnetism(material: Crystal, H: List[float], moment: List[float], T: float) -> Tuple[float, float]:
+    """Fitting magnetic field dependence of magnetic moment to theoretical paramagnetism.
+
+    Note:
+        M = n g J B_J(g muB J H/kB T) [muB/f.u.],
+        where
+            n = number of magnetic atom per formula unit,
+            g = g factor,
+            J = total angular momentum quantum number,
+            B_J = Brillouin function,
+            kB = Boltzmann constant,
+            H = magnetic field,
+            T = temperature.
+    Args:
+        material (Crystal): Crystal instance.
+        H (List[float]): Magnetic field (Oe).
+        moment (List[float]): Magnetic moment (emu).
+
+    Returns:
+        (Tuple[float, float]): g and J.
+    """
+    n: int = material.num_magnetic_ion
+    magnetization = lambda h, g, J: paramagnetization_curie(h, T, g, J, n)
+    popt, pcov = curve_fit(magnetization, np.array(H), moment)
+    return popt
+
+def demagnetizating_factor_ellipsoid(a: float, b: float, c: float) -> Tuple[float]:
+    """Calculating demagnetizating factor of ellipsoid 2a x 2b x 2c.
+
+    Args:
+        a (float): Length of an edge (arb. unit).
+        b (float): Length of an edge (arb. unit).
+        c (float): Length of an edge (arb. unit).
+    
+    Returns:
+        (Tuple[float]): Demagnetizating factor Nx, Ny, Nz.
+    """
+    a, b, c = a/(a+b+c), b/(a+b+c), c/(a+b+c)
+    def D(u: float) -> float:
+        return (a**2+u) * (b**2+u) * (c**2+u)
+    
+    def fx(u: float) -> float:
+        return 1 / ((a**2+u) * np.sqrt(D(u)))
+    def fy(u: float) -> float:
+        return 1 / ((b**2+u) * np.sqrt(D(u)))
+    def fz(u: float) -> float:
+        return 1 / ((c**2+u) * np.sqrt(D(u)))
+    
+    Nx: float = a*b*c/2 * integrate.quad(fx, 0, np.inf)[0]
+    Ny: float = a*b*c/2 * integrate.quad(fy, 0, np.inf)[0]
+    Nz: float = a*b*c/2 * integrate.quad(fz, 0, np.inf)[0]
+    return Nx, Ny, Nz
+
+# ### Nx+Ny+Nz=1を満たさない，スケーリングで値が異なるなどの問題点
+# def demagnetizating_factor_rectangular_prism_Nf(a: float, b: float, c: float) -> float:
+#     """Calculating demagnetizating factor of rectangular prism axbxc.
+
+#     Thesis:
+#         D.-X. Chen et al., IEEE Transactions on Magnetics 38, 4 (2002).
+
+#     Args:
+#         a (float): Length of an edge (arb. unit).
+#         b (float): Length of an edge (arb. unit).
+#         c (float): Length of an edge (arb. unit).
+    
+#     Returns:
+#         (float): Demagnetizating factor.
+#     """
+#     def Ff(u: float, v: float) -> float:
+#         return u * np.log(c**2*(8*u**2+4*v**2+c**2+4*u*np.sqrt(4*u**2+4*v**2+c**2)) / (4*v**2+c**2) / (8*u**2+c**2+4*u*np.sqrt(4*u**2+c**2)))
+
+#     F1: float = np.sqrt(4*a**2+c**2) + np.sqrt(4*b**2+c**2) - np.sqrt(4*a**2+4*b**2+c**2) - c
+#     Nf: float = 2/np.pi * np.arctan(4*a*b/(c*np.sqrt(4*a**2+4*b**2+c**2))) + (c/(2*np.pi*a*b))*(F1+Ff(a,b)+Ff(b,a))
+#     return Nf
+
+# def demagnetizating_factor_rectangular_prism_Nm(a: float, b: float, c: float) -> float:
+#     """Calculating demagnetizating factor of rectangular prism axbxc.
+
+#     Thesis:
+#         D.-X. Chen et al., IEEE Transactions on Magnetics 38, 4 (2002).
+
+#     Args:
+#         a (float): Length of an edge (arb. unit).
+#         b (float): Length of an edge (arb. unit).
+#         c (float): Length of an edge (arb. unit).
+    
+#     Returns:
+#         (float): Demagnetizating factor.
+#     """
+#     def Fm(u: float, v: float, w: float) -> float:
+#         return u**2*v * np.log((u**2+w**2)*(u**2+2*v**2+2*v*np.sqrt(u**2+v**2)) / (u**2) / (u**2+2*v**2+w**2+2*v*np.sqrt(u**2+v**2+w**2)))
+
+#     F2: float = a**3 + b**3 - 2*c**3 + (a**2+b**2-2*c**2)*np.sqrt(a**2+b**2+c**2)
+#     F3: float = (2*c**2-a**2)*np.sqrt(a**2 + c**2) + (2*c**2-b**2)*np.sqrt(b**2 + c**2) - np.sqrt(b**2 + c**2)**3
+
+#     Nm: float = 2/np.pi * np.arctan(a*b/(c*np.sqrt(a**2+b**2+c**2))) + 1/(3*np.pi*a*b*c)*(F2+F3) + 1/(2*np.pi*a*b*c) * (Fm(a,b,c)+Fm(b,a,c)-Fm(c,a,b)-Fm(c,b,a))
+#     return Nm
+
+def demagnetizating_factor_rectangular_prism(a: float, b: float, c: float) -> float:
+    """Calculating demagnetizating factor of rectangular prism axbxc.
+
+    Thesis:
+        A. Aharoni et al., Journal of Applied Physics 83, 3432 (1998).
+
+    Args:
+        a (float): Length of an edge (arb. unit).
+        b (float): Length of an edge (arb. unit).
+        c (float): Length of an edge (arb. unit).
+    
+    Returns:
+        (float): Demagnetizating factor.
+    """
+
+    abc_root: float = np.sqrt(a**2 + b**2 + c**2)
+    ab_root: float = np.sqrt(a**2 + b**2)
+    bc_root: float = np.sqrt(b**2 + c**2)
+    ca_root: float = np.sqrt(a**2 + c**2)
+
+    F1: float = (b**2-c**2) / (2*b*c) * np.log((abc_root-a) / (abc_root+a))
+    F2: float = (a**2-c**2) / (2*a*c) * np.log((abc_root-b) / (abc_root+b))
+    F3: float = b / (2*c) * np.log((ab_root+a) / (ab_root-a))
+    F4: float = a / (2*c) * np.log((ab_root+b) / (ab_root-b))
+    F5: float = c / (2*a) * np.log((bc_root-b) / (bc_root+b))
+    F6: float = c / (2*b) * np.log((ca_root-a) / (ca_root+a))
+    F7: float = 2 * np.arctan(a*b/(c*abc_root))
+    F8: float = (a**3 + b**3 - 2*c**3) / (3 * a * b * c)
+    F9: float = (a**2 + b**2 - 2*c**2) / (3 * a * b * c) * abc_root
+    F10: float = c / (a*b) * (ca_root + bc_root)
+    F11: float = - (ab_root**3 + bc_root**3 + ca_root**3) / (3 * a * b * c)
+    Dz: float = (F1 + F2 + F3 + F4 + F5 + F6 + F7 + F8 + F9 + F10 + F11) / np.pi
+    return Dz
+
+
 
 def main() -> None:
     pass
